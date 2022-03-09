2c366c60542422f4df2a11ed27ffdb687e57f240
Mateusz Szweda <szweda.mateusz@wp.pl>
2022-03-09 18:48:24 +0100

    fix last condition

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function checkNumber(a, b) {
-    if (a === 20 || b === 20 || a + b === 20) {
+    if (a === 20 || b === 20 || a + b <= 20) {
         return true;
     } else {
         return false;
     }
 }
+
+// console.log(checkNumber(1, 2));
+
+checkNumber(1, 2);
